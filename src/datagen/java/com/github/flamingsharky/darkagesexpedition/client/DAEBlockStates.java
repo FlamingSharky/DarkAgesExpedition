a4db1002612f3d857ee44cbaabc9a5f9cfe12afa
Nertzhul <rodrigovazfigueredo@hotmail.com>
2022-06-30 00:43:54 -0300

    Refactor gradle settings Bump parchment and forge versions

---
+++ src/datagen/java/com/github/flamingsharky/darkagesexpedition/client/DAEBlockStates.java
@@ -0,0 +1,33 @@
+package com.github.flamingsharky.darkagesexpedition.client;
+
+import com.github.flamingsharky.darkagesexpedition.DAEMod;
+import com.github.flamingsharky.darkagesexpedition.setup.Registration;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class DAEBlockStates extends BlockStateProvider {
+
+    public DAEBlockStates(DataGenerator gen, ExistingFileHelper helper) {
+        super(gen, DAEMod.MOD_ID, helper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlock(Registration.VENEUM_BLOCK.get());
+        simpleBlock(Registration.VENEUM_ORE.get());
+        simpleBlock(Registration.DEEPSLATE_VENEUM_ORE.get());
+        simpleBlock(Registration.TUFF_VENEUM_ORE.get());
+        // simpleBlock(Registration.RAW_VENEUM_BLOCK.get());
+
+
+        simpleBlock(Registration.SANCTUM_BLOCK.get());
+        // simpleBlock(Registration.SANCTUM_ORE.get());
+        // simpleBlock(Registration.DEEPSLATE_SANCTUM_ORE.get());
+
+        // EVERYTHING COMMENTED OUT NEEDS TEXTURES!!
+
+        // simpleBlock(Registration.TUFF_SANCTUM_ORE.get());
+        // simpleBlock(Registration.RAW_SANCTUM_BLOCK.get());
+    }
+}
---
+++ src/datagen/java/com/github/flamingsharky/darkagesexpedition/common/DAERecipeProvider.java
@@ -0,0 +1,169 @@
+package com.github.flamingsharky.darkagesexpedition.common;
+
+import com.github.flamingsharky.darkagesexpedition.setup.Tool_List;
+import com.github.flamingsharky.darkagesexpedition.setup.Registration;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.util.function.Consumer;
+
+public class DAERecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public DAERecipeProvider(DataGenerator pGenerator) {
+        super(pGenerator);
+    }
+    @Override
+    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+
+        //Veneum Crafting Recipes
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.VENEUM_ORE_ITEM.get()),
+                        Registration.VENEUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.VENEUM_ORE_ITEM.get()))
+                .save(consumer, "veneum_ingot1");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.DEEPSLATE_VENEUM_ORE_ITEM.get()),
+                        Registration.VENEUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.DEEPSLATE_VENEUM_ORE_ITEM.get()))
+                .save(consumer, "veneum_ingot2");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.TUFF_VENEUM_ORE_ITEM.get()),
+                        Registration.VENEUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.TUFF_VENEUM_ORE_ITEM.get()))
+                .save(consumer, "veneum_ingot3");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.RAW_VENEUM.get()),
+                        Registration.VENEUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_chunk", has(Registration.RAW_VENEUM.get()))
+                .save(consumer, "veneum_ingot4");
+
+        ShapedRecipeBuilder.shaped(Registration.VENEUM_BLOCK_ITEM.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.VENEUM_INGOT.get())
+                .group("darkagesexpedition")
+                .unlockedBy("veneum_block1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "veneum_block_shaped");
+
+        ShapedRecipeBuilder.shaped(Registration.RAW_VENEUM_BLOCK_ITEM.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.RAW_VENEUM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("raw_veneum_block1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.RAW_VENEUM.get()))
+                .save(consumer, "raw_veneum_block_shaped");
+
+        ShapedRecipeBuilder.shaped(Registration.VENEUM_INGOT.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.VENEUM_NUGGET.get())
+                .group("darkagesexpedition")
+                .unlockedBy("veneum_ingot5", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "veneum_ingot_shaped");
+
+        ShapelessRecipeBuilder.shapeless(Registration.VENEUM_INGOT.get(), 9)
+                .requires(Registration.VENEUM_BLOCK_ITEM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("veneum_ingot6", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "venenum_ingot1_shapeless");
+
+        ShapelessRecipeBuilder.shapeless(Registration.VENEUM_NUGGET.get(),9)
+                .requires(Registration.VENEUM_INGOT.get())
+                .group("darkagesexpedition")
+                .unlockedBy("veneum_nugget1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "veneum_nugget1_shapeless");
+
+        ShapelessRecipeBuilder.shapeless(Registration.RAW_VENEUM.get(),9)
+                .requires(Registration.RAW_VENEUM_BLOCK_ITEM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("raw_veneum1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.RAW_VENEUM_BLOCK_ITEM.get()))
+                .save(consumer, "veneum_raw_nugget1_shapeless");
+
+        //Veneum Weapons
+        ShapedRecipeBuilder.shaped(Tool_List.VENEUM_LONGSWORD.get())
+                .pattern("  V")
+                .pattern("VV ")
+                .pattern("SV ")
+                .define('V', Registration.VENEUM_INGOT.get())
+                .define('S', Items.STICK)
+                .group("darkagesexpedition")
+                .unlockedBy("veneum_longsword", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "veneum_longsword_shaped");
+
+        //Sanctum
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.SANCTUM_ORE_ITEM.get()),
+                        Registration.SANCTUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.SANCTUM_ORE_ITEM.get()))
+                .save(consumer, "sanctum_ingot1");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.DEEPSLATE_SANCTUM_ORE_ITEM.get()),
+                        Registration.SANCTUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.DEEPSLATE_SANCTUM_ORE_ITEM.get()))
+                .save(consumer, "sanctum_ingot2");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.TUFF_SANCTUM_ORE_ITEM.get()),
+                        Registration.SANCTUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_ore", has(Registration.TUFF_SANCTUM_ORE_ITEM.get()))
+                .save(consumer, "sanctum_ingot3");
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(Registration.RAW_SANCTUM.get()),
+                        Registration.SANCTUM_INGOT.get(), 1.0f, 200)
+                .unlockedBy("has_chunk", has(Registration.RAW_SANCTUM.get()))
+                .save(consumer, "sanctum_ingot4");
+
+        ShapedRecipeBuilder.shaped(Registration.SANCTUM_BLOCK_ITEM.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.SANCTUM_INGOT.get())
+                .group("darkagesexpedition")
+                .unlockedBy("sanctum_block1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SANCTUM_INGOT.get()))
+                .save(consumer, "sanctum_block_shaped");
+
+        ShapedRecipeBuilder.shaped(Registration.RAW_SANCTUM_BLOCK_ITEM.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.RAW_SANCTUM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("raw_sanctum_block1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.RAW_SANCTUM.get()))
+                .save(consumer, "raw_sanctum_block_shaped");
+
+        ShapedRecipeBuilder.shaped(Registration.SANCTUM_INGOT.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', Registration.SANCTUM_NUGGET.get())
+                .group("darkagesexpedition")
+                .unlockedBy("sanctum_ingot5", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SANCTUM_INGOT.get()))
+                .save(consumer, "sanctum_ingot_shaped");
+
+        ShapelessRecipeBuilder.shapeless(Registration.SANCTUM_INGOT.get(), 9)
+                .requires(Registration.SANCTUM_BLOCK_ITEM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("sanctum_ingot6", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SANCTUM_INGOT.get()))
+                .save(consumer, "sanctum_ingot_shapeless1");
+
+        ShapelessRecipeBuilder.shapeless(Registration.SANCTUM_NUGGET.get(),9)
+                .requires(Registration.SANCTUM_INGOT.get())
+                .group("darkagesexpedition")
+                .unlockedBy("sanctum_nugget1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SANCTUM_INGOT.get()))
+                .save(consumer, "sanctum_nugget_shapeless");
+
+        ShapelessRecipeBuilder.shapeless(Registration.RAW_SANCTUM.get(),9)
+                .requires(Registration.RAW_SANCTUM_BLOCK_ITEM.get())
+                .group("darkagesexpedition")
+                .unlockedBy("raw_sanctum1", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.RAW_SANCTUM_BLOCK_ITEM.get()))
+                .save(consumer, "raw_sanctum_shapeless");
+
+        //Sanctum Weapons
+        ShapedRecipeBuilder.shaped(Tool_List.SANCTUM_LONGSWORD.get())
+                .pattern("  V")
+                .pattern("VV ")
+                .pattern("SV ")
+                .define('V', Registration.SANCTUM_INGOT.get())
+                .define('S', Items.STICK)
+                .group("darkagesexpedition")
+                .unlockedBy("sanctum_longsword", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.VENEUM_INGOT.get()))
+                .save(consumer, "sanctum_longsword_shaped");
+    }
+}
